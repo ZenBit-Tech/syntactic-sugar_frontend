5e858d82a6003a72ca918cc9c0ec21d520c74d2e
Ksenia Komisarova <cardavar.ksen@gmail.com>
2022-11-03 20:02:16 +0200

    add start files

--- apps/freelance/src/app/app.tsx
+++ apps/freelance/src/app/app.tsx
@@ -1,6 +1,4 @@
 import styled from 'styled-components';
-import { Route,  Routes } from 'react-router-dom';
-import HomePage from '../pages/HomePage/HomePage';
 import NxWelcome from './nx-welcome';
 
 const StyledApp = styled.div`
