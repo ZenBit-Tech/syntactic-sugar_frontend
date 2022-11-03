7e6c236e1a409fb97a6a8ef2014d21b088d5b70f
Ksenia Komisarova <cardavar.ksen@gmail.com>
2022-11-03 19:57:28 +0200

    add start files

--- apps/freelance/src/app/app.tsx
+++ apps/freelance/src/app/app.tsx
@@ -1,27 +1,18 @@
 import styled from 'styled-components';
-import { Routes, Route } from 'react-router-dom';
+import { Route,  Routes } from 'react-router-dom';
 import HomePage from '../pages/HomePage/HomePage';
 import NxWelcome from './nx-welcome';
 
 const StyledApp = styled.div`
-  // Your style here 
+  /* Your style here  */
 `
 
-
-interface RoutesProps {
-  children?: React.ReactNode;
-  location?: Partial<Location> | string;
-}
-
-export function App(props: RoutesProps) {
+export function App( ) {
   return (
     <StyledApp>
     <NxWelcome title="Welcome freelance" />
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
-    </StyledApp>
+    </StyledApp> 
   );
-}
+  }
 
 export default App;
--- apps/freelance/src/main.tsx
+++ apps/freelance/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import App from './app/app';
+import HomePage from './pages/HomePage/HomePage';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +12,9 @@ root.render(
   <StrictMode>
     <BrowserRouter>
     <App />
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+    </Routes>
     </BrowserRouter>
   </StrictMode>
 );
