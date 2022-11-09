57d912b2886afb06ff2ee328ffc01c5211d9ff00
braulioportela79 <braulioportela@outlook.com>
2022-11-09 16:36:12 -0300

    fix: removed commented code

--- apps/freelance/src/main.tsx
+++ apps/freelance/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { Suspense } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
-// import ExamplePage from './pages/example-page/example-page';
 import './i18n';
 import { ThemeProvider } from 'styled-components';
 import { ThemeColors, GlobalStyle } from './styles/global.styled';
--- libs/components/src/lib/example-header/example-header.tsx
+++ libs/components/src/lib/example-header/example-header.tsx
@@ -1,16 +1,11 @@
-// import { Header } from 'antd/lib/layout/layout';
-// import { Typography } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { StyledHeader } from './example-header.styled';
 import { StyledTitle } from '../../../../../apps/freelance/src/styles/typograghy';
 import { ThemeColors } from '../../../../../apps/freelance/src/styles/global.styled';
 import { ThemeProvider } from 'styled-components';
-// import 'antd/dist/antd.css';
 
 /* eslint-disable-next-line */
 export interface ExampleHeaderProps {}
-
-// const { Title } = Typography;
 
 export function ExampleHeader(props: ExampleHeaderProps) {
   const { t } = useTranslation();
