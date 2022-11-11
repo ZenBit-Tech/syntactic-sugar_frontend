1c97c8ef10e1f5a564bb5c67f09d8a9b7857ed84
braulioportela79 <braulioportela@outlook.com>
2022-11-11 11:29:14 -0300

    fix: background color

--- apps/freelance/src/pages/signup-page/signup-page.tsx
+++ apps/freelance/src/pages/signup-page/signup-page.tsx
@@ -1,14 +1,18 @@
 import { StyledPage } from './signup-page.styled';
 import { SignupForm } from '@freelance/components';
+import { ThemeProvider } from 'styled-components';
+import { ThemeColors } from '@freelance/components';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface SignUpPageProps {}
 
 export function SignUpPage(props: SignUpPageProps) {
   return (
-    <StyledPage>
-      <SignupForm />
-    </StyledPage>
+    <ThemeProvider theme={ThemeColors}>
+      <StyledPage>
+        <SignupForm />
+      </StyledPage>
+    </ThemeProvider>
   );
 }
 
