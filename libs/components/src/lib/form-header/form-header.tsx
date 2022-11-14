87650497f8a19491125fad115e4a2908cf636340
braulioportela79 <braulioportela@outlook.com>
2022-11-14 13:49:42 -0300

    refactor: form container style / form header

--- libs/components/src/lib/form-header/form-header.tsx
+++ libs/components/src/lib/form-header/form-header.tsx
@@ -1,3 +1,4 @@
+import { Container } from './form-header.styled';
 import { StyledTitle, StyledParagraph } from '@freelance/components';
 
 /* eslint-disable-next-line */
@@ -9,13 +10,15 @@ export interface FormHeaderProps {
 
 export function FormHeader({ title, subTitle, isSignForm }: FormHeaderProps) {
   return (
-    <>
+    <Container>
       <StyledTitle tag="h2" fontWeight={800} fontSize="md">
         {title}
       </StyledTitle>
       {isSignForm && <img id="logo" src="/assets/images/logo.png" alt="logo" />}
-      <StyledParagraph fontSize="md"><strong>{subTitle}</strong></StyledParagraph>
-    </>
+      <StyledParagraph fontSize="md">
+        <strong>{subTitle}</strong>
+      </StyledParagraph>
+    </Container>
   );
 }
 
