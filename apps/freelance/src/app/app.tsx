f6b6f23a68894bb79945044d7cc059a57f286c16
braulioportela79 <braulioportela@outlook.com>
2022-11-15 11:30:25 -0300

    feature: add signup page ui

--- apps/freelance/src/app/app.tsx
+++ apps/freelance/src/app/app.tsx
@@ -1,6 +1,7 @@
 import { StyledApp } from './app.styled';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from '@pages/login-page/login-page';
+import SignupPage from '@pages/signup-page/signup-page';
 
 export function App() {
   return (
@@ -8,6 +9,7 @@ export function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
         </Routes>
       </BrowserRouter>
     </StyledApp>
