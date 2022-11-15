b06800e4da4ee7fe98e0039f32d10568ec89c3ae
Деркачев Иван <derkachov_ivan@host-192-168-0-102.sohonet.ua>
2022-11-15 22:18:36 +0200

    feature: login by email and login by Google

--- apps/freelance/src/utils/validations/loginForm.ts
+++ apps/freelance/src/utils/validations/loginForm.ts
@@ -5,7 +5,7 @@ export const signInSchema = object({
     .email("Email must be a valid email address")
     .required("Email is required"),
   password: string()
-    .min(5, 'minimum is 5')
+    .min(8, 'minimum is 8')
     .max(24, 'maximum is 24')
     .required('password is required')
 });
