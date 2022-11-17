aea61a2077b292b6bf85d7f4ca1107006808ee70
Ksenia Komisarova <cardavar.ksen@gmail.com>
2022-11-17 04:28:21 +0200

    fix: types rtq

--- libs/components/src/lib/form-container/form-containerHooks.tsx
+++ libs/components/src/lib/form-container/form-containerHooks.tsx
@@ -3,19 +3,15 @@ import { useNavigate } from "react-router-dom";
 import { useSignUpMutation } from "redux/signup-googleApi";
  
 export const  useGoogleAuthentication = () => {
-    const navigate = useNavigate();
-    const [signupUser] = useSignUpMutation();
+  const navigate = useNavigate();
+  const [signupUser] = useSignUpMutation();
 
-    const handleSuccess = useGoogleLogin({
-        onSuccess:  tokenResponse => {
-          signupUser({ token: tokenResponse.access_token });
-          navigate("/role");
+  const handleSuccess = useGoogleLogin({
+      onSuccess: tokenResponse => {
+        signupUser({ token: tokenResponse.access_token });
+        navigate("/role");
         }
       })
+  return { handleSuccess };
 
-    return {
-    handleSuccess,
-  }
 };
-
-
