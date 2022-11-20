8e4492dd878d78cae2286592ab4d025dcbc151b7
Деркачев Иван <derkachov_ivan@host-192-168-0-100.sohonet.ua>
2022-11-20 22:12:58 +0200

    feature: login by email and bu Google

--- apps/freelance/src/redux/store.ts
+++ apps/freelance/src/redux/store.ts
@@ -1,14 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from './example-slice';
+import { loginApi } from "./login.api";
 import {signupGoogleApi} from './signup-googleApi';
 
 const store = configureStore({
     reducer: {
       user: userReducer,
       [signupGoogleApi.reducerPath]: signupGoogleApi.reducer,
+      [loginApi.reducerPath]: loginApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(signupGoogleApi.middleware),
+    getDefaultMiddleware().concat(signupGoogleApi.middleware, loginApi.middleware),
 });
 
 export default store;
