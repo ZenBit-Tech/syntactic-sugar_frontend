3b26253623d72cd44d10519b1bd827c9cd51d3ee
Ksenia Komisarova <cardavar.ksen@gmail.com>
2022-11-28 11:04:26 +0200

    feature: add base markup

--- libs/components/src/lib/example-header/example-header.tsx
+++ libs/components/src/lib/example-header/example-header.tsx
@@ -1,24 +1,25 @@
-import { useTranslation } from 'react-i18next';
-import { StyledHeader } from './example-header.styled';
-import { StyledTitle } from '@freelance/components';
-import { ThemeColors } from '@freelance/components';
-import { ThemeProvider } from 'styled-components';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { StyledHeader } from "./example-header.styled";
+import { StyledTitle } from "@freelance/components";
+import { ThemeColors } from "@freelance/components";
+import { ThemeProvider } from "styled-components";
 
 /* eslint-disable-next-line */
 export interface ExampleHeaderProps {}
 
 export function ExampleHeader(props: ExampleHeaderProps) {
-  const { t } = useTranslation();
+	const { t } = useTranslation();
 
-  return (
-    <ThemeProvider theme={ThemeColors}>
-      <StyledHeader>
-        <StyledTitle tag="h1" fontWeight={800} fontSize="lg">
-          {t('Hello, Syntactic sugar!')}
-        </StyledTitle>
-      </StyledHeader>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={ThemeColors}>
+			<StyledHeader>
+				<StyledTitle tag="h1" fontWeight={800} fontSize="lg">
+					{t("Hello, Syntactic sugar!")}
+				</StyledTitle>
+			</StyledHeader>
+		</ThemeProvider>
+	);
 }
 
 export default ExampleHeader;
--- libs/components/src/lib/send-proposal-freelancer/send-proposal-freelancer.styled.tsx
+++ libs/components/src/lib/send-proposal-freelancer/send-proposal-freelancer.styled.tsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
 
-
-
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+`;
