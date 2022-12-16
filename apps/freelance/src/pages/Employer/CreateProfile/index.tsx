91589baf2e633e99cff16cc21ba14ee006df7961
Dmytro Chushko <dmytro.chushko@gmail.com>
2022-12-16 11:20:40 +0200

    has fixed create profile page

--- apps/freelance/src/pages/Employer/CreateProfile/index.tsx
+++ apps/freelance/src/pages/Employer/CreateProfile/index.tsx
@@ -47,9 +47,8 @@ export function CreateEmployerProfile() {
 			aboutUs: values.aboutUs,
 		};
 		try {
-			console.log(employerInfo);
 			await createProfile(employerInfo);
-			// navigate(MY_JOBS);
+			navigate(MY_JOBS);
 		} catch (error) {
 			alert(error);
 		}
