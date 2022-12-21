412d46bc92ae192bd42523c9f03c756d8e993381
Ksenia Komisarova <cardavar.ksen@gmail.com>
2022-12-21 14:53:28 +0200

    fix: delete unused var

--- apps/freelance/src/pages/WorkDetails/index.tsx
+++ apps/freelance/src/pages/WorkDetails/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate, useParams } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
