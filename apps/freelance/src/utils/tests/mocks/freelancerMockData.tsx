52811e50a24d01bf9b08110731e20b92142ce71a
Ksenia Komisarova <cardavar.ksen@gmail.com>
2022-12-28 15:42:58 +0200

    feature: add tests

---
+++ apps/freelance/src/utils/tests/mocks/freelancerMockData.tsx
@@ -0,0 +1,32 @@
+export const mockFreelancerData = {
+	id: "1",
+	fullName: "Iryna",
+	category: { id: "2", name: "Retail" },
+	position: "Full stack",
+	skills: [{ id: "3", name: "Java" }],
+	employmentType: "Office",
+	country: { id: "uk", name: "Ukraine" },
+	hourRate: "Less than 500$",
+	availableAmountOfHours: "Part time",
+	workExperience: "2 years",
+	englishLevel: "Beginner",
+	education: [
+		{
+			company: "Company",
+			workPosition: "Sales manager",
+			period: "2008-2013",
+		},
+	],
+	workHistory: [
+		{
+			institute: "NUFT",
+			occupation: "Master",
+			period: "2008-2013",
+		},
+	],
+	otherExperience: "Other experience",
+	isPublished: false,
+	image: "url/img",
+	proposals: [{ id: "1", coverLetter: "coverLetter" }],
+	user: { id: "1", email: "email.com" },
+};
