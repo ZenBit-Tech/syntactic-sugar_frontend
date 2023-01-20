92c80e8cf8dd5ebcb0bc66680d72e1319fdda2c1
Ksenia Komisarova <cardavar.ksen@gmail.com>
2023-01-20 09:22:49 +0200

    fix

--- apps/freelance/src/pages/Employer/CreateProfile/index.tsx
+++ apps/freelance/src/pages/Employer/CreateProfile/index.tsx
@@ -12,6 +12,13 @@ import {
 	Dashboard,
 	StyledTitle,
 	StyledButton,
+	EditForm,
+	ErrorsHandlerWrapper,
+	Input,
+	GridContainer,
+	StyledSpan,
+	EditProfileTextArea,
+	ButtonContainer,
 } from "@freelance/components";
 import { StyledPage } from "@pages/Freelancer/CreateProfile1/style";
 import { DEFAULT_IMAGE, EMPLOYER_JOBS_PAGE } from "utils/constants/links";
@@ -111,179 +118,222 @@ export function CreateEmployerProfile() {
 					<StyledTitle tag="h2" fontSize="sm" fontWeight={500}>
 						{t("employer.create.title")}
 					</StyledTitle>
-					<FormBox onSubmit={handleSubmit(onSubmit)}>
-						<StyledFileField>
-							<img src={imageUrl} alt="User Avatar" />
-							<div>
-								<label>
-									{t("image.chooseImage")}
-									<input
-										id="fileInput"
-										type="file"
-										accept=".png, .jpg, .jpeg"
-										{...register("image")}
-										onChange={onSubmitFile}
-									/>
-								</label>
-								<label>
-									{t("image.resetImage")}
-									<input
-										type="button"
-										onClick={() => {
-											setImageUrl(DEFAULT_IMAGE);
-										}}
-									/>
-								</label>
-							</div>
-						</StyledFileField>
-						<InputContainer>
-							<Controller
-								name="fullName"
-								control={control}
-								rules={{ required: "This field is required." }}
-								render={({ field }) => (
-									<input
-										id="fullName"
-										type="text"
-										{...field}
-										placeholder={t("employer.create.fullNameLabel")}
-									/>
-								)}
-							/>
-							<ErrorMessage
-								errors={errors}
-								name="fullName"
-								render={({ message }) => <span>{message}</span>}
-							/>
-						</InputContainer>
-						<InputContainer>
-							<Controller
-								name="position"
-								control={control}
-								rules={{ required: "This field is required." }}
-								render={({ field }) => (
-									<input
-										type="text"
-										{...field}
-										id="position"
-										placeholder={t("employer.create.positionLabel")}
-									/>
-								)}
-							/>
-							<ErrorMessage
-								errors={errors}
-								name="position"
-								render={({ message }) => <span>{message}</span>}
-							/>
-						</InputContainer>
-						<InputContainer>
-							<Controller
-								name="companyName"
-								control={control}
-								rules={{ required: "This field is required." }}
-								render={({ field }) => (
-									<input
-										type="text"
-										id="companyName"
-										{...field}
-										placeholder={t("employer.create.companyNameLabel")}
-									/>
-								)}
-							/>
-							<ErrorMessage
-								errors={errors}
-								name="companyName"
-								render={({ message }) => <span>{message}</span>}
-							/>
-						</InputContainer>
-						<InputContainer>
-							<Controller
-								name="phone"
-								control={control}
-								rules={{
-									required: "This field is required.",
-									pattern: {
-										value: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
-										message: "Add correct number, example: +919367788755",
-									},
-								}}
-								render={({ field }) => (
-									<input
-										type="tel"
-										{...field}
-										id="phone"
-										placeholder={t("employer.create.phoneLabel")}
-									/>
-								)}
-							/>
-							<ErrorMessage
-								errors={errors}
-								name="phone"
-								render={({ messages }) =>
-									messages &&
-									Object.entries(messages).map(([type, message]) => (
-										<span key={type}>{message}</span>
-									))
-								}
-							/>
-						</InputContainer>
-						<InputContainer>
-							<Controller
-								name="linkedIn"
-								control={control}
-								rules={{ required: "This field is required." }}
-								render={({ field }) => (
-									<input
-										type="text"
-										{...field}
-										id="linkedIn"
-										placeholder={t("employer.create.linkedinLabel")}
-									/>
-								)}
-							/>
-							<ErrorMessage
-								errors={errors}
-								name="linkedIn"
-								render={({ message }) => <span>{message}</span>}
-							/>
-						</InputContainer>
-						<InputContainer>
-							<Controller
-								name="website"
-								control={control}
-								rules={{ required: "This field is required." }}
-								render={({ field }) => (
-									<input
-										type="text"
-										{...field}
-										id="website"
-										placeholder={t("employer.create.websiteLabel")}
-									/>
-								)}
-							/>
-							<ErrorMessage
-								errors={errors}
-								name="website"
-								render={({ message }) => <span>{message}</span>}
-							/>
-						</InputContainer>
-						<Controller
-							name="aboutUs"
-							control={control}
-							rules={{ required: "This field is required." }}
-							render={({ field }) => (
-								<textarea {...field} placeholder={t("employer.create.aboutusLabel")} />
-							)}
-						/>
-						<ErrorMessage
-							errors={errors}
-							name="aboutUs"
-							render={({ message }) => <span>{message}</span>}
-						/>
-						<StyledButton type="submit" buttonColor="redGradient" buttonSize="sm" fontSize="md">
-							<strong>{t("employer.create.btn1")}</strong>
-						</StyledButton>
-					</FormBox>
+					<EditForm onSubmit={handleSubmit(onSubmit)}>
+						<GridContainer gap={10}>
+							<StyledFileField>
+								<img src={imageUrl} alt="User Avatar" />
+								<div>
+									<label>
+										{t("image.chooseImage")}
+										<input
+											id="fileInput"
+											type="file"
+											accept=".png, .jpg, .jpeg"
+											{...register("image")}
+											onChange={onSubmitFile}
+										/>
+									</label>
+									<label>
+										{t("image.resetImage")}
+										<input
+											type="button"
+											onClick={() => {
+												setImageUrl(DEFAULT_IMAGE);
+											}}
+										/>
+									</label>
+								</div>
+							</StyledFileField>
+							<ErrorsHandlerWrapper positionRight={-21} width={18}>
+								<Controller
+									name="fullName"
+									control={control}
+									rules={{ required: "This field is required." }}
+									render={({ field }) => (
+										<Input
+											id="fullName"
+											type="text"
+											{...field}
+											placeholder={t("employer.create.fullNameLabel")}
+											width={100}
+										/>
+									)}
+								/>
+								<ErrorMessage
+									errors={errors}
+									name="fullName"
+									render={({ message }) => (
+										<StyledSpan fontSize="sm" type="validation">
+											<strong>{message}</strong>
+										</StyledSpan>
+									)}
+								/>
+							</ErrorsHandlerWrapper>
+							<ErrorsHandlerWrapper positionRight={-21} width={18}>
+								<Controller
+									name="position"
+									control={control}
+									rules={{ required: "This field is required." }}
+									render={({ field }) => (
+										<Input
+											type="text"
+											{...field}
+											id="position"
+											placeholder={t("employer.create.positionLabel")}
+											width={100}
+										/>
+									)}
+								/>
+								<ErrorMessage
+									errors={errors}
+									name="position"
+									render={({ message }) => (
+										<StyledSpan fontSize="sm" type="validation">
+											<strong>{message}</strong>
+										</StyledSpan>
+									)}
+								/>
+							</ErrorsHandlerWrapper>
+							<ErrorsHandlerWrapper positionRight={-21} width={18}>
+								<Controller
+									name="companyName"
+									control={control}
+									rules={{ required: "This field is required." }}
+									render={({ field }) => (
+										<Input
+											type="text"
+											id="companyName"
+											{...field}
+											placeholder={t("employer.create.companyNameLabel")}
+											width={100}
+										/>
+									)}
+								/>
+								<ErrorMessage
+									errors={errors}
+									name="companyName"
+									render={({ message }) => (
+										<StyledSpan fontSize="sm" type="validation">
+											<strong>{message}</strong>
+										</StyledSpan>
+									)}
+								/>
+							</ErrorsHandlerWrapper>
+							<ErrorsHandlerWrapper positionRight={-21} width={18}>
+								<Controller
+									name="phone"
+									control={control}
+									rules={{
+										required: "This field is required.",
+										pattern: {
+											value: /^[\.]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
+											message: "Add correct number, example: +919367788755",
+										},
+									}}
+									render={({ field }) => (
+										<Input
+											type="tel"
+											{...field}
+											id="phone"
+											placeholder={t("employer.create.phoneLabel")}
+											width={100}
+										/>
+									)}
+								/>
+								<ErrorMessage
+									errors={errors}
+									name="phone"
+									render={({ messages }) =>
+										messages &&
+										Object.entries(messages).map(([type, message]) => (
+											<StyledSpan fontSize="sm" type="validation">
+												<strong>{message}</strong>
+											</StyledSpan>
+										))
+									}
+								/>
+							</ErrorsHandlerWrapper>
+							<ErrorsHandlerWrapper positionRight={-21} width={18}>
+								<Controller
+									name="linkedIn"
+									control={control}
+									rules={{ required: "This field is required." }}
+									render={({ field }) => (
+										<Input
+											type="text"
+											{...field}
+											id="linkedIn"
+											placeholder={t("employer.create.linkedinLabel")}
+											width={100}
+										/>
+									)}
+								/>
+								<ErrorMessage
+									errors={errors}
+									name="linkedIn"
+									render={({ message }) => (
+										<StyledSpan fontSize="sm" type="validation">
+											<strong>{message}</strong>
+										</StyledSpan>
+									)}
+								/>
+							</ErrorsHandlerWrapper>
+							<ErrorsHandlerWrapper positionRight={-21} width={18}>
+								<Controller
+									name="website"
+									control={control}
+									rules={{ required: "This field is required." }}
+									render={({ field }) => (
+										<Input
+											type="text"
+											{...field}
+											id="website"
+											placeholder={t("employer.create.websiteLabel")}
+											width={100}
+										/>
+									)}
+								/>
+								<ErrorMessage
+									errors={errors}
+									name="website"
+									render={({ message }) => (
+										<StyledSpan fontSize="sm" type="validation">
+											<strong>{message}</strong>
+										</StyledSpan>
+									)}
+								/>
+							</ErrorsHandlerWrapper>
+							<ErrorsHandlerWrapper positionRight={-21} width={18}>
+								<Controller
+									name="aboutUs"
+									control={control}
+									rules={{ required: "This field is required." }}
+									render={({ field }) => (
+										<EditProfileTextArea
+											{...field}
+											placeholder={t("employer.create.aboutusLabel")}
+											rows={5}
+											maxLength={600}
+										/>
+									)}
+								/>
+								<ErrorMessage
+									errors={errors}
+									name="aboutUs"
+									render={({ message }) => (
+										<StyledSpan fontSize="sm" type="validation">
+											<strong>{message}</strong>
+										</StyledSpan>
+									)}
+								/>
+							</ErrorsHandlerWrapper>
+							<ButtonContainer align="center">
+								<StyledButton type="submit" buttonColor="redGradient" buttonSize="sm" fontSize="md">
+									<strong>{t("employer.create.btn1")}</strong>
+								</StyledButton>
+							</ButtonContainer>
+						</GridContainer>
+					</EditForm>
 					<ToastContainer />
 				</Dashboard>
 			</StyledPage>
