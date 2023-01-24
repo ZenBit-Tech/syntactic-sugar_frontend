8e846ce3d94fbfd69d27a9e38cbdb97d7536d5eb
Ksenia Komisarova <cardavar.ksen@gmail.com>
2023-01-24 09:33:33 +0200

    fix: delete duplicate numbers

--- apps/freelance/src/utils/validations/editEmployerSchema.ts
+++ apps/freelance/src/utils/validations/editEmployerSchema.ts
@@ -5,6 +5,9 @@ import { useJobsValidationErrorMessages } from "utils/constants/jobs-validation-
 export const useEditEmployerSchema = (): SchemaOf<IEditEmployerForm> => {
 	const {
 		FIELD_REQUIRED,
+		INPUT_CHAR_NUMBER,
+		PHONE_TWELVE_CHAR_NUMBER,
+		TEXTAREA_CHAR_NUMBER,
 		MESSAGE_JOB_TITLE_MAX_CHAR,
 		CORRECT_PHONE,
 		MIN_TWELVE_CHAR,
@@ -14,25 +17,29 @@ export const useEditEmployerSchema = (): SchemaOf<IEditEmployerForm> => {
 	} = useJobsValidationErrorMessages();
 
 	return object({
-		fullName: string().max(50, MESSAGE_JOB_TITLE_MAX_CHAR).required(FIELD_REQUIRED),
-		position: string().max(50, MESSAGE_JOB_TITLE_MAX_CHAR).required(FIELD_REQUIRED),
-		companyName: string().max(50, MESSAGE_JOB_TITLE_MAX_CHAR).required(FIELD_REQUIRED),
+		fullName: string().max(INPUT_CHAR_NUMBER, MESSAGE_JOB_TITLE_MAX_CHAR).required(FIELD_REQUIRED),
+		position: string().max(INPUT_CHAR_NUMBER, MESSAGE_JOB_TITLE_MAX_CHAR).required(FIELD_REQUIRED),
+		companyName: string()
+			.max(INPUT_CHAR_NUMBER, MESSAGE_JOB_TITLE_MAX_CHAR)
+			.required(FIELD_REQUIRED),
 		phone: string()
 			.required(FIELD_REQUIRED)
-			.max(50, MESSAGE_JOB_TITLE_MAX_CHAR)
-			.min(12, MIN_TWELVE_CHAR)
+			.max(INPUT_CHAR_NUMBER, MESSAGE_JOB_TITLE_MAX_CHAR)
+			.min(PHONE_TWELVE_CHAR_NUMBER, MIN_TWELVE_CHAR)
 			.matches(/^[\\+]?[(]?[0-9]{3}[)]?[-\s\\.]?[0-9]{3}[-\s\\.]?[0-9]{4,6}/gm, CORRECT_PHONE),
 		linkedIn: string()
-			.max(50, MESSAGE_JOB_TITLE_MAX_CHAR)
+			.max(INPUT_CHAR_NUMBER, MESSAGE_JOB_TITLE_MAX_CHAR)
 			.required(FIELD_REQUIRED)
 			.matches(
 				/^(http(s)?:\/\/)?([\w]+\.)?linkedin\.com\/(pub|in|profile)/gm,
 				LINKED_IN_ERROR_MESSAGE,
 			),
 		website: string()
-			.max(50, MESSAGE_JOB_TITLE_MAX_CHAR)
+			.max(INPUT_CHAR_NUMBER, MESSAGE_JOB_TITLE_MAX_CHAR)
 			.required(FIELD_REQUIRED)
 			.matches(/(^http[s]?:\/{2})|(^www)|(^\/{1,2})/gim, WEBSITE_ERROR_MESSAGE),
-		aboutUs: string().required(FIELD_REQUIRED).max(600, MESSAGE_DESCRIPTION_MAX_CHAR),
+		aboutUs: string()
+			.required(FIELD_REQUIRED)
+			.max(TEXTAREA_CHAR_NUMBER, MESSAGE_DESCRIPTION_MAX_CHAR),
 	});
 };
